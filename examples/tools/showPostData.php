75d59e171f582a7462a2222b77ff01e9f1fbc3ba
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-07-23 15:10:15 -0400

    issue-119 - Actually unrelated to this issue.  This simpler program shows all the request headers.  Since more and more of the headers were becoming interesting, it seems like all of them should be shown.

--- examples/tools/showPostData.php
+++ examples/tools/showPostData.php
@@ -1,10 +1,8 @@
 <?php
-/** @var string[] $headers */
-$headers = getallheaders();
-$rawPostData = file_get_contents('php://input');
+foreach (getallheaders() as $name => $value) {
+    error_log("${name}: ${value}");
+}
 
-error_log('Authorization: ' . @$headers['Authorization']);
-error_log('Content-Type: ' . @$headers['Content-Type']);
-error_log('User-Agent: ' . @$headers['User-Agent']);
-error_log("Raw post data:\n" . $rawPostData);
+$rawPostData = file_get_contents('php://input');
+error_log("Raw post data:\n${rawPostData}");
 error_log('Received ' . strlen($rawPostData) . ' bytes');
