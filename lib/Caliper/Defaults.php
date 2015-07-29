ee29ad7cc471b39a59936b8f57132286c2fc5de7
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-07-29 16:15:44 -0400

    issue-119 - Added support for JsonInclude options.

--- lib/Caliper/Defaults.php
+++ lib/Caliper/Defaults.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'Caliper/util/BasicEnum.php';
+require_once 'Caliper/util/JsonInclude.php';
 
 class Defaults extends BasicEnum {
     const
@@ -8,6 +9,7 @@ class Defaults extends BasicEnum {
         CONNECTION_REQUEST_TIMEOUT = 10000,
         CONNECTION_TIMEOUT = 10000,
         SOCKET_TIMEOUT = 10000,
+        JSON_INCLUDE = JsonInclude::ALWAYS,
         JSON_ENCODE_OPTIONS = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES,
         DEBUG = false;
 }
