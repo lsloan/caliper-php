6c07447b1cdf0a54a662f45c8ce983ab6c1dc1a3
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-01-19 14:16:00 -0500

    IMSGlobal/caliper-php#180 - Removed `navigatedFrom` support, per clarified IMSGlobal/caliper-central#25

--- src/events/NavigationEvent.php
+++ src/events/NavigationEvent.php
@@ -1,44 +1,12 @@
 <?php
 namespace IMSGlobal\Caliper\events;
 
-use IMSGlobal\Caliper\entities;
 use IMSGlobal\Caliper\actions;
 
 class NavigationEvent extends Event {
-    /**
-     * @deprecated 1.2 See `Event.referrer`
-     * @var entities\DigitalResource
-     */
-    private $navigatedFrom;
-
     public function __construct() {
         parent::__construct();
         $this->setType(new EventType(EventType::NAVIGATION))
             ->setAction(new actions\Action(actions\Action::NAVIGATED_TO));
     }
-
-    public function jsonSerialize() {
-        return array_merge(parent::jsonSerialize(), [
-            'navigatedFrom' => $this->getNavigatedFrom(),
-        ]);
-    }
-
-    /**
-     * @deprecated 1.2 See `Event.referrer`
-     * @return entities\DigitalResource navigatedFrom
-     */
-    public function getNavigatedFrom() {
-        return $this->navigatedFrom;
-    }
-
-    /**
-     * @deprecated 1.2 See `Event.referrer`
-     * @param entities\DigitalResource $navigatedFrom
-     * @return $this|NavigationEvent
-     */
-    public function setNavigatedFrom(entities\DigitalResource $navigatedFrom) {
-        $this->navigatedFrom = $navigatedFrom;
-        return $this;
-    }
 }
-
