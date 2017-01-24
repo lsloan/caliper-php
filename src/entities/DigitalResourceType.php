3068f771146a5d62c8ff8e415d94fc4390ee903e
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-01-24 00:36:38 -0500

    IMSGlobal/caliper-php#187 - Simplified entity types, per IMSGlobal/caliper-central#81

--- src/entities/DigitalResourceType.php
+++ src/entities/DigitalResourceType.php
@@ -4,14 +4,14 @@ namespace IMSGlobal\Caliper\entities;
 class DigitalResourceType extends \IMSGlobal\Caliper\util\BasicEnum implements Type {
     const
         __default = '',
-        ASSIGNABLE_DIGITAL_RESOURCE = 'http://purl.imsglobal.org/caliper/v1/AssignableDigitalResource',
-        EPUB_CHAPTER = 'http://purl.imsglobal.org/caliper/v1/EpubChapter',
-        EPUB_PART = 'http://purl.imsglobal.org/caliper/v1/EpubPart',
-        EPUB_SUB_CHAPTER = 'http://purl.imsglobal.org/caliper/v1/EpubSubChapter',
-        EPUB_VOLUME = 'http://purl.imsglobal.org/caliper/v1/EpubVolume',
-        FRAME = 'http://purl.imsglobal.org/caliper/v1/Frame',
-        MEDIA_OBJECT = 'http://purl.imsglobal.org/caliper/v1/MediaObject',
-        MEDIA_LOCATION = 'http://purl.imsglobal.org/caliper/v1/MediaLocation',
-        READING = 'http://purl.imsglobal.org/caliper/v1/Reading',
-        WEB_PAGE = 'http://purl.imsglobal.org/caliper/v1/WebPage';
+        ASSIGNABLE_DIGITAL_RESOURCE = 'AssignableDigitalResource',
+        EPUB_CHAPTER = 'EpubChapter',
+        EPUB_PART = 'EpubPart',
+        EPUB_SUB_CHAPTER = 'EpubSubChapter',
+        EPUB_VOLUME = 'EpubVolume',
+        FRAME = 'Frame',
+        MEDIA_OBJECT = 'MediaObject',
+        MEDIA_LOCATION = 'MediaLocation',
+        READING = 'Reading',
+        WEB_PAGE = 'WebPage';
 }
