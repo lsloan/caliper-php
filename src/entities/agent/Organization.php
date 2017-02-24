3ead3260a86bb1ad084d97a68fb3735bad75c6d7
Lance E Sloan «UMich» <lsloan-github.com@umich.edu>
2017-02-24 18:06:01 -0500

    IMSGlobal/caliper-php#240 - Add Organization.members (#260)
    IMSGlobal/caliper-php#240 - Add Organization.members
    * Added `Organization.members`.  Also added `jsonSerialize()` to implement `@​content` reduction.
    * Simplified `Group`.  It needed new parentage and a reduction in its methods.
    
    Per IMSGlobal/caliper-central#73 and IMSGlobal/caliper-central#74.

--- src/entities/agent/Organization.php
+++ src/entities/agent/Organization.php
@@ -6,10 +6,46 @@ use IMSGlobal\Caliper\entities;
 class Organization extends entities\Entity implements entities\foaf\Agent, entities\w3c\Organization {
     /** @var entities\w3c\Organization */
     private $subOrganizationOf;
+    /** @var entities\foaf\Agent[] */
+    private $members;
 
     public function __construct($id) {
         parent::__construct($id);
         $this->setType(new entities\EntityType(entities\EntityType::ORGANIZATION));
+    }
+
+    public function jsonSerialize() {
+        return $this->removeChildEntitySameContexts(array_merge(parent::jsonSerialize(), [
+            'members' => $this->getMembers(),
+            'subOrganizationOf' => $this->getSubOrganizationOf(),
+        ]));
+
+    }
+
+    /** @return entities\foaf\Agent[] */
+    public function getMembers() {
+        return $this->members;
+    }
+
+    /**
+     * @param entities\foaf\Agent[] $members
+     * @return $this
+     */
+    public function setMembers($members) {
+        if (!is_null($members)) {
+            if (!is_array($members)) {
+                $members = [$members];
+            }
+
+            foreach ($members as $member) {
+                if (!($member instanceof entities\foaf\Agent)) {
+                    throw new \InvalidArgumentException(__METHOD__ . ': array of Agent expected');
+                }
+            }
+        }
+        
+        $this->members = $members;
+        return $this;
     }
 
     /** @return entities\w3c\Organization */
