a04738d75ff64da96f83fefcc7d8be6ecb06d01d
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-03-01 15:41:13 -0500

    IMSGlobal/caliper-php#238 - Deprecate Epub and Reading code.

--- src/entities/DigitalResourceType.php
+++ src/entities/DigitalResourceType.php
@@ -7,12 +7,12 @@ class DigitalResourceType extends \IMSGlobal\Caliper\util\BasicEnum implements T
         ASSIGNABLE_DIGITAL_RESOURCE = 'AssignableDigitalResource',
         CHAPTER = 'Chapter',
         DOCUMENT = 'Document',
-        EPUB_CHAPTER = 'EpubChapter',
-        EPUB_PART = 'EpubPart',
-        EPUB_VOLUME = 'EpubVolume',
+        EPUB_CHAPTER = 'EpubChapter', // @deprecated 1.2
+        EPUB_PART = 'EpubPart', // @deprecated 1.2
+        EPUB_VOLUME = 'EpubVolume', // @deprecated 1.2
         FRAME = 'Frame',
         MEDIA_OBJECT = 'MediaObject',
         MEDIA_LOCATION = 'MediaLocation',
-        READING = 'Reading',
+        READING = 'Reading', // @deprecated 1.2
         WEB_PAGE = 'WebPage';
 }
