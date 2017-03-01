2fb5ee260245dc6476652d4f5b837d9374a9fbde
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-03-01 13:05:37 -0500

    IMSGlobal/caliper-php#167 - Coercion needed more work than expected. I'm interested to see how other Caliper implementations handle this.  Adding new contexts wasn't difficult.  It was trickier to use those contexts to coerce the values of serialized objects to other values.  This implementation works specifically with the context given in the test fixture and may not be flexible enough to handle other contexts.  Technically, it shouldn't be necessary to define and use `TestContext::NULL` here, but the child objects were getting the default Caliper context without it.  It probably indicates that the context reduction code needs improvement.  Also reformatted code.

--- tests/EnvelopeEventViewViewedCoercedTest.php
+++ tests/EnvelopeEventViewViewedCoercedTest.php
@@ -1,103 +1,101 @@
 <?php
+use IMSGlobal\Caliper\actions\Action;
+use IMSGlobal\Caliper\context\Context;
+use IMSGlobal\Caliper\entities\agent\Person;
+use IMSGlobal\Caliper\entities\agent\SoftwareApplication;
+use IMSGlobal\Caliper\entities\lis\CourseSection;
+use IMSGlobal\Caliper\entities\lis\Membership;
+use IMSGlobal\Caliper\entities\lis\Role;
+use IMSGlobal\Caliper\entities\lis\Status;
+use IMSGlobal\Caliper\entities\reading\Document;
+use IMSGlobal\Caliper\entities\session\Session;
+use IMSGlobal\Caliper\events\ViewEvent;
+use IMSGlobal\Caliper\request\Envelope;
+use IMSGlobal\Caliper\Sensor;
+
 require_once 'CaliperTestCase.php';
 
+class TestContext extends Context {
+    const
+        __default = null,
+        NULL = null,
+        EVENT = [
+        'http://purl.imsglobal.org/ctx/caliper/v1p1',
+        [
+            'actor' => [
+                'id' => 'Person',
+                'type' => 'id',
+            ],
+        ],
+        [
+            'edApp' => [
+                'id' => 'SoftwareApplication',
+                'type' => 'id',
+            ],
+        ],
+        [
+            'object' => [
+                'id' => 'Document',
+                'type' => 'id',
+            ],
+        ],
+    ],
+        MEMBERSHIP = [
+        [
+            'member' => [
+                'id' => 'Person',
+                'type' => 'id',
+            ],
+        ],
+    ];
+}
 
 /**
  * @requires PHP 5.4
  */
 class EnvelopeEventViewViewedCoercedTest extends CaliperTestCase {
+
+
     function setUp() {
         parent::setUp();
 
-        $this->setTestObject(
-            (
-            array(
-                'sensor' => 'https://example.edu/sensors/1',
-                'sendTime' => '2016-11-15T11:05:01.000Z',
-                'dataVersion' => 'http://purl.imsglobal.org/ctx/caliper/v1p1',
-                'data' =>
-                    array(
-                        0 =>
-                            (array(
-                                '@context' =>
-                                    array(
-                                        0 => 'http://purl.imsglobal.org/ctx/caliper/v1p1',
-                                        1 =>
-                                            (array(
-                                                'assignee' =>
-                                                    (array(
-                                                        'id' => 'Person',
-                                                        'type' => 'id',
-                                                    )),
-                                            )),
-                                        2 =>
-                                            (array(
-                                                'edApp' =>
-                                                    (array(
-                                                        'id' => 'SoftwareApplication',
-                                                        'type' => 'id',
-                                                    )),
-                                            )),
-                                        3 =>
-                                            (array(
-                                                'object' =>
-                                                    (array(
-                                                        'id' => 'Document',
-                                                        'type' => 'id',
-                                                    )),
-                                            )),
-                                    ),
-                                'type' => 'ViewEvent',
-                                'assignee' => 'https://example.edu/users/554433',
-                                'action' => 'Viewed',
-                                'object' => 'https://example.edu/etexts/201.epub',
-                                'eventTime' => '2016-11-15T10:15:00.000Z',
-                                'edApp' => 'https://example.edu',
-                                'group' =>
-                                    (array(
-                                        'id' => 'https://example.edu/terms/201601/courses/7/sections/1',
-                                        'type' => 'CourseSection',
-                                        'courseNumber' => 'CPS 435-01',
-                                        'academicSession' => 'Fall 2016',
-                                    )),
-                                'membership' =>
-                                    (array(
-                                        '@context' =>
-                                            array(
-                                                0 =>
-                                                    (array(
-                                                        'member' =>
-                                                            (array(
-                                                                'id' => 'Person',
-                                                                'type' => 'id',
-                                                            )),
-                                                    )),
-                                            ),
-                                        'id' => 'https://example.edu/terms/201601/courses/7/sections/1/rosters/1',
-                                        'type' => 'Membership',
-                                        'member' => 'https://example.edu/users/554433',
-                                        'organization' =>
-                                            (array(
-                                                'id' => 'https://example.edu/terms/201601/courses/7/sections/1',
-                                                'type' => 'CourseSection',
-                                            )),
-                                        'roles' =>
-                                            array(
-                                                0 => 'Learner',
-                                            ),
-                                        'status' => 'Active',
-                                        'dateCreated' => '2016-08-01T06:00:00.000Z',
-                                    )),
-                                'session' =>
-                                    (array(
-                                        'id' => 'https://example.edu/sessions/1f6442a482de72ea6ad134943812bff564a76259',
-                                        'type' => 'Session',
-                                        'startedAtTime' => '2016-11-15T10:00:00.000Z',
-                                    )),
-                                'uuid' => 'e6f54458-19e3-47e3-bc18-43f231c74a45',
-                            )),
-                    ),
-            ))
+        $this->setTestObject((new Envelope())
+            ->setSensorId(new Sensor('https://example.edu/sensors/1'))
+            ->setSendTime(new \DateTime('2016-11-15T11:05:01.000Z'))
+            ->setData([
+                (new ViewEvent())
+                    ->setContext(new TestContext(TestContext::EVENT))
+                    ->setActor((new Person('https://example.edu/users/554433')))
+                    ->setAction(new Action(Action::VIEWED))
+                    ->setObject((new Document('https://example.edu/etexts/201.epub'))
+                        ->setName('IMS Caliper Implementation Guide')
+                        ->setDateCreated(new \DateTime('2016-08-01T06:00:00.000Z'))
+                        ->setDatePublished(new \DateTime('2016-10-01T06:00:00.000Z'))
+                        ->setVersion('1.1')
+                    )
+                    ->setEventTime(new \DateTime('2016-11-15T10:15:00.000Z'))
+                    ->setEdApp((new SoftwareApplication('https://example.edu')))
+                    ->setGroup((new CourseSection('https://example.edu/terms/201601/courses/7/sections/1'))
+                        ->setContext(new TestContext(TestContext::NULL))
+                        ->setCourseNumber('CPS 435-01')
+                        ->setAcademicSession('Fall 2016')
+                    )
+                    ->setMembership((new Membership('https://example.edu/terms/201601/courses/7/sections/1/rosters/1'))
+                        ->setContext(new TestContext(TestContext::MEMBERSHIP))
+                        ->setMember((new Person('https://example.edu/users/554433')))
+                        ->setOrganization((new CourseSection('https://example.edu/terms/201601/courses/7/sections/1'))
+                            ->setContext(new TestContext(TestContext::NULL))
+                        )
+                        ->setRoles([new Role(Role::LEARNER)])
+                        ->setStatus(new Status(Status::ACTIVE))
+                        ->setDateCreated(new \DateTime('2016-08-01T06:00:00.000Z'))
+                    )
+                    ->setSession((new Session('https://example.edu/sessions/1f6442a482de72ea6ad134943812bff564a76259'))
+                        ->setContext(new TestContext(TestContext::NULL))
+                        ->setStartedAtTime(new \DateTime('2016-11-15T10:00:00.000Z'))
+                    )
+                    ->setUuid('e6f54458-19e3-47e3-bc18-43f231c74a45'),
+            ])
         );
     }
 }
