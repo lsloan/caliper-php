e6d3ae116e5ff87587c412e3fb7dfa1ee63cd34b
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-03-01 13:39:33 -0500

    IMSGlobal/caliper-php#167 - Corrected entity envelope tests. Because these tests were passing, I didn't notice that they were not written correctly.  They had been generated by my JSON to PHP program, which didn't interpret the contents of `Envelope` objects correctly, so it would produce code to represent their contents as nested arrays.  These now call the actual Caliper code for each class.

--- tests/EnvelopeEntitySingleTest.php
+++ tests/EnvelopeEntitySingleTest.php
@@ -1,6 +1,10 @@
 <?php
-require_once 'CaliperTestCase.php';
+use IMSGlobal\Caliper\entities\agent\Person;
+use IMSGlobal\Caliper\entities\DigitalResource;
+use IMSGlobal\Caliper\entities\DigitalResourceCollection;
+use IMSGlobal\Caliper\entities\lis\CourseSection;
 
+require_once 'CaliperTestCase.php';
 
 /**
  * @requires PHP 5.4
@@ -9,44 +13,22 @@ class EnvelopeEntitySingleTest extends CaliperTestCase {
     function setUp() {
         parent::setUp();
 
-        $this->setTestObject(
-            (
-            array(
-                'sensor' => 'https://example.edu/sensors/1',
-                'sendTime' => '2016-11-15T11:05:01.000Z',
-                'dataVersion' => 'http://purl.imsglobal.org/ctx/caliper/v1p1',
-                'data' =>
-                    array(
-                        0 =>
-                            (array(
-                                '@context' => 'http://purl.imsglobal.org/ctx/caliper/v1p1',
-                                'id' => 'https://example.edu/terms/201601/courses/7/sections/1/resources/1/syllabus.pdf',
-                                'type' => 'DigitalResource',
-                                'name' => 'Course Syllabus',
-                                'mediaType' => 'application/pdf',
-                                'creators' =>
-                                    array(
-                                        0 =>
-                                            (array(
-                                                'id' => 'https://example.edu/users/223344',
-                                                'type' => 'Person',
-                                            )),
-                                    ),
-                                'isPartOf' =>
-                                    (array(
-                                        'id' => 'https://example.edu/terms/201601/courses/7/sections/1/resources/1',
-                                        'type' => 'DigitalResourceCollection',
-                                        'name' => 'Course Assets',
-                                        'isPartOf' =>
-                                            (array(
-                                                'id' => 'https://example.edu/terms/201601/courses/7/sections/1',
-                                                'type' => 'CourseSection',
-                                            )),
-                                    )),
-                                'dateCreated' => '2016-08-02T11:32:00.000Z',
-                            )),
-                    ),
-            ))
+        $this->setTestObject((new \IMSGlobal\Caliper\request\Envelope())
+            ->setSensorId(new \IMSGlobal\Caliper\Sensor('https://example.edu/sensors/1'))
+            ->setSendTime(new \DateTime('2016-11-15T11:05:01.000Z'))
+            ->setData([
+                (new DigitalResource('https://example.edu/terms/201601/courses/7/sections/1/resources/1/syllabus.pdf'))
+                    ->setName('Course Syllabus')
+                    ->setMediaType('application/pdf')
+                    ->setCreators([
+                        (new Person('https://example.edu/users/223344')),
+                    ])
+                    ->setIsPartOf((new DigitalResourceCollection('https://example.edu/terms/201601/courses/7/sections/1/resources/1'))
+                        ->setName('Course Assets')
+                        ->setIsPartOf((new CourseSection('https://example.edu/terms/201601/courses/7/sections/1')))
+                    )
+                    ->setDateCreated(new \DateTime('2016-08-02T11:32:00.000Z')),
+            ])
         );
     }
 }
