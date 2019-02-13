8abd8ac4fa7b5dd5c3438cd6da15e2e665f11d9c
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2019-02-13 13:00:02 -0500

    remove pretty JSON default setting
    Not directly related to requestor extension support, but the JSON pretty print option needed to be removed from the default set.  It's too small of a change to warrant its own branch.

--- src/Defaults.php
+++ src/Defaults.php
@@ -10,6 +10,6 @@ class Defaults extends util\BasicEnum {
         JSON_INCLUDE = util\JsonInclude::NON_EMPTY,
         // TODO: Make PHP 5.6 required, to support math with constants.
         // TODO: Make PHP 5.6.6 required, to support `JSON_PRESERVE_ZERO_FRACTION` constant.
-        JSON_ENCODE_OPTIONS = 1216, // = JSON_UNESCAPED_SLASHES (64) | JSON_PRETTY_PRINT (128) | JSON_PRESERVE_ZERO_FRACTION (1024), // (required new const syntax of PHP 5.6)
+        JSON_ENCODE_OPTIONS = 1088, // = JSON_UNESCAPED_SLASHES (64) | JSON_PRESERVE_ZERO_FRACTION (1024), // (required new const syntax of PHP 5.6)
         DEBUG = false;
 }
