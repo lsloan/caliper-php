c06e7625752added81a0854af0617bb4c3f6b399
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2019-02-13 13:11:36 -0500

    initial requestor extension support attempt
    This is probably the minimum needed to support setting and using an extension of a Requestor object.

--- src/Client.php
+++ src/Client.php
@@ -1,15 +1,19 @@
 <?php
+
 namespace IMSGlobal\Caliper;
 
 use IMSGlobal\Caliper\entities\Entity;
 use IMSGlobal\Caliper\events\Event;
 use IMSGlobal\Caliper\request\HttpRequestor;
+use IMSGlobal\Caliper\request\Requestor;
 
 class Client {
     /** @var string */
     private $id;
     /** @var Options */
     private $options;
+    /** @var Requestor */
+    private $requestor = null;
 
     /**
      * @param string $id
@@ -33,6 +37,7 @@ class Client {
         $this->id = $id;
         return $this;
     }
+
 
     /**
      * Send application events
@@ -92,5 +97,28 @@ class Client {
 
         (new HttpRequestor($this->getOptions()))
             ->send($sensor, $entities);
+    }
+
+    /**
+     * Return the current Requestor.  If `$initializeIfNull` is `true` and no requestor
+     * is already set, initialize it with an instance of `HttpRequestor` using the
+     * current options.
+     *
+     * @return Requestor
+     */
+    public function getRequestor($initializeIfNull = false) {
+        if (($initializeIfNull === true) && is_null($this->requestor)) {
+            $this->requestor = new HttpRequestor($this->getOptions());
+        }
+        return $this->requestor;
+    }
+
+    /**
+     * @param Requestor $requestor
+     * @return $this|Client
+     */
+    public function setRequestor($requestor) {
+        $this->requestor = $requestor;
+        return $this;
     }
 }
